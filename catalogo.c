25b1bb931c850902c9af5bd3e39da988d1e8cb96
João Bento <joaovictorbento.s.b@gmail.com>
2026-08-16 22:49:11 -0300

    Adicionando músicas e realizando paginação do catalogo

--- catalogo.c
+++ catalogo.c
@@ -1,34 +1,75 @@
 #include "funcoes.h"
 
-
 void catalogo(ListaMusica *listaMusica){
     
     ApontadorMusica p;
+    ApontadorMusica pagina[100];
+    int paginaAtual=0;
+    int avancar=1;
     int i;
-
-    system("cls");
-    telaCatalogo();
+    int opcao;
+    int contador;
 
     ordenarCatalogo(listaMusica);
 
-    p = listaMusica->primeiro;
+    pagina[0] = listaMusica->primeiro;
 
-    i=6;
-    while(p != NULL){
 
-        gotoxy(3,i);
-        printf("%d", p->conteudo.id);
+    while(avancar) {
+        system("cls");
+        telaCatalogo();
 
-        gotoxy(8,i);
-        printf("%s", p->conteudo.musica);
+        p = pagina[paginaAtual];
+        i=7;
+        contador=0;
+        while(p != NULL && contador < 12){
 
-        i++;
-        p = p->proximo;
+            gotoxy(3,i);
+            printf("%d", p->conteudo.id);
 
+            gotoxy(8,i);
+            printf("%s", p->conteudo.musica);
+
+            i++;
+            p = p->proximo;
+            contador++;
+
+        }
+
+        limpa_msg();
+        gotoxy(2,23);
+        printf("[<] VOLTAR - AVANCAR [>] - SAIR [0]");
+
+        opcao = getch();
+
+
+        if(opcao == 224){
+
+            opcao=getch();
+
+            switch(opcao){
+                case 77:
+                    if(p != NULL && paginaAtual < 99){
+                        paginaAtual++;
+                        pagina[paginaAtual] = p;
+                    }
+                    break;
+                case 75:
+                    if(paginaAtual > 0){
+                        paginaAtual--;
+                    }
+                    break;
+            }
+        }else{
+            if(opcao == 0){
+                return;
+            }else{
+                system("cls");
+                limpa_msg();
+                printf("OPCAO INVALIDA......");
+                getch();   
+                return;   
+            }
+        }
     }
-
-    getch();
-
-
-
 }
